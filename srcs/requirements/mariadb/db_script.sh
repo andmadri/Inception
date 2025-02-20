af670a9501e9f5430e84b120c01f3c27745eebbd
Andres Madrid Ucha <andmadri@f0r3s25.codam.nl>
2025-02-20 13:58:46 +0100

    wait_for_mariadb() function and commented out the healthcheck

--- srcs/requirements/mariadb/db_script.sh
+++ srcs/requirements/mariadb/db_script.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+wait_for_maridab() {
+    while ! nc -z mariadb $DB_PORT; does
+        echo "Waiting for MariaDB to be ready..."
+        sleep 5
+    done
+    echo "MariaDB is ready!"
+}
+
+wait_for_maridab
+
 if [ ! 'find /var/lib/mysql -name $DATABASE_NAME' ]; then
     service mariadb start
     echo "Databse $DB_NAME does not exist, creating it..."
-
-    #change owner to mysq; (R -recursive - all directories and fikes)
-
-
     mysql << EOF
     CREATE DATABASE IF NOT EXISTS $DB_NAME ;
     CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_USR_PWD' ;
