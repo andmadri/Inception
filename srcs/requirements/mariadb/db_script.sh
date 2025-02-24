698c3fc2a6e89908b6bdefae5d42bed2bb9a34b2
andmadri <andresmadridu@outlook.com>
2025-02-24 14:47:00 +0100

    is the aborted connection message okay

--- srcs/requirements/mariadb/db_script.sh
+++ srcs/requirements/mariadb/db_script.sh
@@ -6,15 +6,18 @@ mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 chmod -R 755 /var/run/mysqld
 
+echo "Creating database"
 service mariadb start
-sleep 4
+sleep 2
 mysql << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME ;
 CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_USR_PWD' ;
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%' ;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%' IDENTIFIED BY '$DB_USR_PWD' ;
+FLUSH PRIVILEGES ;
 EOF
-
 service mariadb stop
+echo "Database created"
+
+sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 exec "$@"
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -12,24 +12,23 @@ wait_for_mariadb
 
 if [ ! -f "/var/www/html/wp-load.php" ]; then
 	echo "Downloading Wordpress..."
-	wp core download --path="/var/www/html/" --allow-root
+	wp-cli.phar core download --path="/var/www/html/" --allow-root
 	echo "Wordpress is Downloaded..."
+	echo "Creating wp-config.php..."
+	wp-cli.phar config create --path="/var/www/html/" --dbname="$DB_NAME" --dbuser="$DB_USR" --dbpass="$DB_USR_PWD" --dbhost="$DB_HOST" --allow-root
 else
 	echo "Wordpress is already downloaded..."
 fi
 
-echo "Creating wp-config.php..."
-wp config create --path="/var/www/html/" --dbname="$DB_NAME" --dbuser="$DB_USR" --dbpass="$DB_USR_PWD" --dbhost="$DB_HOST":"$DB_PORT" --allow-root
-echo "Created wp-config.php..."
-
-if ! wp core is-installed --path="/var/www/html/" --allow-root; then
+if ! wp-cli.phar core is-installed --path="/var/www/html/" --allow-root; then
 	echo "Installing Wordpress..."
-	wp core install --path="/var/www/html/" --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USR" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+	wp-cli.phar core install --path="/var/www/html/" --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USR" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 fi
 
-if ! wp user get "$WP_USR" --path="/var/www/html/" --allow-root; then
+if ! wp-cli.phar user get "$WP_USR" --path="/var/www/html/" --allow-root > /dev/null 2>&1; then
 	echo "Creating $WP_USR user ..."
-	wp user create "$WP_USR" "$WP_USR_EMAIL" --path="/var/www/html/" --user_pass="$WP_USR_PWD" --allow-root
+	wp-cli.phar user create "$WP_USR" "$WP_USR_EMAIL" --path="/var/www/html/" --user_pass="$WP_USR_PWD" --allow-root
 fi
 
+echo "Executing WordPress..."
 exec "$@"
